f9d81f62b6eacf92066bb35a691973d2ec3bc21b
Haren-Sharma <harensharma33@gmail.com>
2025-06-28 16:11:13 +0530

    added the dark theme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,15 @@
 import { Slot } from "expo-router";
+import { ThemeProvider, DarkTheme } from "@react-navigation/native";
 
 export default function RootLayout() {
+  const myTheme = {
+    ...DarkTheme,
+    colors: {
+      ...DarkTheme.colors,
+      primary: "white",
+      card:"#101010",//background color of the header and footer
+    },
+  };
   /*
     Slot:
     a placeholder where child routes (screens) will be rendered
@@ -8,5 +17,9 @@ export default function RootLayout() {
     It’s similar to children in React, but more powerful
     because it's aware of the routing structure.
    */
-  return <Slot />;
+  return (
+    <ThemeProvider value={myTheme}>
+      <Slot />
+    </ThemeProvider>
+  );
 }
